d2ee4717c10fc224205a82ff8e3b855034bf693c
Roman <sselbstmord@gmail.com>
2023-10-09 19:24:31 +0300

    little work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './wrapper.module.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
+      <Wrapper>
+        <Card>
+            i
+        </Card>
+      </Wrapper>
 
-    </div>
+
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(63, 62, 62, 0.19);
+  height: 100vh;
+  width: 100vw;
+`
+
+const Card = styled.div `
+width: 550px;
+  height: 600px;
+  background-color: #fff;
+  border-radius: 13px;
+`
 
 export default App;
